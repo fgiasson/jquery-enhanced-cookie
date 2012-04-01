088d9e30a1352072cf9d850de60e7782eb1c007c
creage <creage@gmail.com>
2012-04-01 19:25:43 +0300

    Update jquery.enhanced.cookie.js

--- jquery.enhanced.cookie.js
+++ jquery.enhanced.cookie.js
@@ -1,4 +1,36 @@
 /**
+
+# $.ms, inspired by ms.js, https://github.com/guille/ms.js
+
+No more painful `setTimeout(fn, 60 * 4 * 3 * 2 * 1 * Infinity * NaN * '☃')`.
+
+    $.ms('2d')      // 172800000
+    $.ms('1.5h')    // 5400000
+    $.ms('1h')      // 3600000
+    $.ms('1m')      // 60000
+    $.ms('5s')      // 5000
+    $.ms('500ms')   // 500
+    $.ms('100')     // 100
+    $.ms(100)       // 100
+
+**/
+(function($){
+  var r = /(\d*.?\d+)([mshd]+)/
+    , _ = {}
+    , m;
+
+  _.ms = 1;
+  _.s = 1000;
+  _.m = _.s * 60;
+  _.h = _.m * 60;
+  _.d = _.h * 24;
+
+  $.ms = function (s) {
+    return +s || ((m = r.exec(s.toLowerCase())) ? m[1] * _[m[2]] : NaN);
+  };
+})(jQuery);
+
+/**
  * jQuery Extended Cookie Plugin
  *
  * Author: Frederick Giasson
